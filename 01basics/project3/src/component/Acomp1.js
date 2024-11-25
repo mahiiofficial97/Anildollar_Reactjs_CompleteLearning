356618676e8b6f822df0b71e4a74dc80c98e7aad
maheshhandee <mbhande88@gmail.com>
2024-11-25 10:17:20 +0530

    P

---
+++ 01basics/project3/src/component/Acomp1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Acomp1() {
+  return (
+    <>
+      <span>mycomponumnet 1</span>
+    </>
+  );
+}
+
+export default Acomp1;
---
+++ 01basics/project3/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(
+  document.querySelector("section.root_element")
+);
+root.render(<h1>hello mahiii </h1>);
